29aa2e093cea0cfb836f7ae06996359dff089afe
Nick Meinhold <nick@enspyr.co>
2022-03-10 19:09:06 +1100

    Update to latest integration_test instructions
    https://github.com/flutter/flutter/tree/master/packages/integration_test

--- integration_test/screenshot_test.dart
+++ integration_test/screenshot_test.dart
@@ -1,17 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:screenshots_test/main.dart';
+import 'package:screenshots_test/main.dart' as app;
 
 void main() {
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized()
+      as IntegrationTestWidgetsFlutterBinding;
+
   testWidgets('screenshot', (WidgetTester tester) async {
-    final binding = IntegrationTestWidgetsFlutterBinding();
-    IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+    // Build the app.
+    app.main();
 
-    await tester.pumpWidget(const MyApp());
+    // This is required prior to taking the screenshot (Android only).
+    // await binding.convertFlutterSurfaceToImage();
+
+    // Trigger a frame.
     await tester.pumpAndSettle();
-
-    await binding.takeScreenshot('test-screenshot');
-
-    // TODO: Add screenshot test
+    await binding.takeScreenshot('screenshot-1');
   });
 }
