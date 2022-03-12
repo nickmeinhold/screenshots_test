ca9b5f8d4d0ce237d9427f8f7835f849e7cae4b9
Rick Poleshuck <RickPoleshuck@gmail.com>
2022-03-11 19:06:21 -0600

    more complete test of ios

--- integration_test/screenshot_test.dart
+++ integration_test/screenshot_test.dart
@@ -1,25 +1,36 @@
 import 'dart:io';
 
+import 'package:screenshots_test/main.dart' as app;
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:screenshots_test/main.dart' as app;
 
 void main() {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized()
-      as IntegrationTestWidgetsFlutterBinding;
+  as IntegrationTestWidgetsFlutterBinding;
+  String platform = Platform.isAndroid ? 'android' : 'ios';
 
   testWidgets('screenshot', (WidgetTester tester) async {
-    // Build the app.
     app.main();
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    late String emulatorName;
+    if (Platform.isAndroid) {
+      // This is required prior to taking the screenshot (Android only).
+      await binding.convertFlutterSurfaceToImage();
 
-    // This is required prior to taking the screenshot (Android only).
-    // await binding.convertFlutterSurfaceToImage();
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      emulatorName = androidInfo.model.toString();
+    } else {
+      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      emulatorName = iosInfo.name!;
+    }
+    final screenshotDir = '/tmp/screenshots/$platform/$emulatorName';
+    Directory(screenshotDir).createSync(recursive: true);
 
-    // Trigger a frame.
     await tester.pumpAndSettle();
     var screenshotBytes = await binding.takeScreenshot('screenshot-1');
 
-    final File image = File('/tmp/screenshot.png');
+    final File image = File('$screenshotDir/first_screen.png');
     image.writeAsBytesSync(screenshotBytes);
   });
 }
